bebdf99080c7cf6421f64724ef253dfb9e29abce
Goran Peretin <goran.peretin@gmail.com>
2011-11-01 14:03:40 +0100

    Request listener delegates parsing to RequestParser

--- request_listener.py
+++ request_listener.py
@@ -1,10 +1,14 @@
 from gevent.wsgi import WSGIServer
+from request_parser import RequestParser
 
 def listen(env, start_response):
     """
     Basic POST request listener
     Just forwards request payload to RequestParser
+    Response is irrelevant, nobody's listening...
     """
+    request_parser = RequestParser(env)
+    request_parser.parse()
     response_headers = [('Content-Type', 'text/plain')]
     start_response('200 OK', response_headers)
     return ""
