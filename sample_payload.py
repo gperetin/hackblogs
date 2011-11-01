505d89430c003ee7273354d9b0d7c91ef0eb00fd
Goran Peretin <goran.peretin@gmail.com>
2011-11-01 13:54:41 +0100

    Added complex_payload to payload samples file

---
+++ sample_payload.py
@@ -0,0 +1,95 @@
+simple_payload = 'payload={"before":"0000000000000000000000000000000000000000",\
+"created":true,"after":"34cf062ffd53a7ee750b2cb032bfddf26ee76caa",\
+"repository":{\
+"has_wiki":true,\
+"has_issues":true,\
+"forks":1,"watchers":1,\
+"open_issues":0,\
+"created_at":"2011/06/26 13:40:43 -0700",\
+"fork":false,\
+"pushed_at":"2011/08/18 10:43:47 -0700",\
+"has_downloads":true,\
+"private":false,\
+"size":0,\
+"owner":{\
+"email":"goran.peretin@gmail.com",\
+"name":"gperetin"},\
+"name":"hackblogs_test",\
+"homepage":"",\
+"url":"https://github.com/gperetin/hackblogs_test",\
+"description":"This repository is used for testing blog_updater blog engine"},\
+"forced":true,\
+"deleted":false,\
+"commits":\
+[{"added":["test1"],\
+"message":"Test1",\
+"removed":["file5.py", "file6.py"],\
+"distinct":true,\
+"modified":["file4.py", "file3.py"],\
+"timestamp":"2011-08-18T10:43:17-07:00",\
+"url":"https://github.com/gperetin/hackblogs_test/commit/34cf062ffd53a7ee750b2cb032bfddf26ee76caa",\
+"id":"34cf062ffd53a7ee750b2cb032bfddf26ee76caa",\
+"author":{\
+"email":"goran.peretin@gmail.com",\
+"username":"gperetin",\
+"name":"Goran Peretin"}}],\
+"pusher":{\
+"email":"goran.peretin@gmail.com",\
+"name":"gperetin"},\
+"base_ref":null,\
+"compare":"https://github.com/gperetin/hackblogs_test/compare/34cf062^...34cf062",\
+"ref":"refs/heads/master"}'
+
+complex_payload = 'payload={"before":"0000000000000000000000000000000000000000",\
+"created":true,"after":"34cf062ffd53a7ee750b2cb032bfddf26ee76caa",\
+"repository":{\
+"has_wiki":true,\
+"has_issues":true,\
+"forks":1,"watchers":1,\
+"open_issues":0,\
+"created_at":"2011/06/26 13:40:43 -0700",\
+"fork":false,\
+"pushed_at":"2011/08/18 10:43:47 -0700",\
+"has_downloads":true,\
+"private":false,\
+"size":0,\
+"owner":{\
+"email":"goran.peretin@gmail.com",\
+"name":"gperetin"},\
+"name":"hackblogs_test",\
+"homepage":"",\
+"url":"https://github.com/gperetin/hackblogs_test",\
+"description":"This repository is used for testing blog_updater blog engine"},\
+"forced":true,\
+"deleted":false,\
+"commits":\
+[{"added":["test1"],\
+"message":"Test1",\
+"removed":["file5.py", "file6.py"],\
+"distinct":true,\
+"modified":["file4.py", "file3.py"],\
+"timestamp":"2011-08-18T10:43:17-07:00",\
+"url":"https://github.com/gperetin/hackblogs_test/commit/34cf062ffd53a7ee750b2cb032bfddf26ee76caa",\
+"id":"34cf062ffd53a7ee750b2cb032bfddf26ee76caa",\
+"author":{\
+"email":"goran.peretin@gmail.com",\
+"username":"gperetin",\
+"name":"Goran Peretin"}},\
+{"added":["test4"],\
+"message":"Second commit",\
+"removed":["file5.py", "file7.py"],\
+"distinct":true,\
+"modified":["file4.py", "file3.py", "file5.py"],\
+"timestamp":"2011-08-18T10:45:17-07:00",\
+"url":"https://github.com/gperetin/hackblogs_test/commit/34cf062ffd53a7ee750b2cb032bfddf26ee76caa",\
+"id":"34cf062ffd53a7ee750b3cb332bfddf26ee76caa",\
+"author":{\
+"email":"goran.peretin@gmail.com",\
+"username":"gperetin",\
+"name":"Goran Peretin"}}],\
+"pusher":{\
+"email":"goran.peretin@gmail.com",\
+"name":"gperetin"},\
+"base_ref":null,\
+"compare":"https://github.com/gperetin/hackblogs_test/compare/34cf062^...34cf062",\
+"ref":"refs/heads/master"}'
